76875ee040c6b066fb1cb7f093c2f6e2710e6ca4
ayeshagh9 <syedsamiulla192181@gmail.com>
2026-09-17 10:29:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -17,3 +17,15 @@ if (menuToggle && navLinks) {
     });
 
 }
+
+if ("serviceWorker" in navigator) {
+    window.addEventListener("load", function () {
+        navigator.serviceWorker.register("./service-worker.js")
+            .then(function () {
+                console.log("Rainbow School app is ready.");
+            })
+            .catch(function (error) {
+                console.log("Service Worker registration failed:", error);
+            });
+    });
+}
